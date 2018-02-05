95adb6a2a6baf52508d7eb32b126ddf3c6d57dd6
rzhang56 <rzhang56@illinois.edu>
2018-02-05 16:43:59 -0600

    added carry select adder

--- Lab 4/ece385sp16_lab4_adders/carry_select_adder.sv
+++ Lab 4/ece385sp16_lab4_adders/carry_select_adder.sv
@@ -11,5 +11,11 @@ module carry_select_adder
      * Insert code here to implement a carry select.
      * Your code should be completly combinational (don't use always_ff or always_latch).
      * Feel free to create sub-modules or other files. */
+     logic c1, c2, c3;
+
+     carry_select_adder4 CS1(.A (A[3:0]), .B (B[3:0]), .C_in (1'b0), .S (Sum[3:0]), .C_out (c1));
+     carry_select_adder4 CS2(.A (A[7:4]), .B (B[7:4]), .C_in (c1), .S (Sum[7:4]), .C_out (c2));
+     carry_select_adder4 CS3(.A (A[11:8]), .B (B[11:8]), .C_in (c2), .S (Sum[11:8]), .C_out (c3));
+     carry_select_adder4 CS4(.A (A[15:12]), .B (B[15:12]), .C_in (c3), .S (Sum[15:12]), .C_out (c2));
      
 endmodule
