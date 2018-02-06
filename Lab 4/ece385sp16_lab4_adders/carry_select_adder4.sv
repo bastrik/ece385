15a21b0d9c4d0f0141564826418ae6c80eda527c
Justin Song <jmsong4@illinois.edu>
2018-02-06 11:17:05 -0600

    Fixed carry-select adder

--- Lab 4/ece385sp16_lab4_adders/carry_select_adder4.sv
+++ Lab 4/ece385sp16_lab4_adders/carry_select_adder4.sv
@@ -2,22 +2,25 @@ module carry_select_adder4
 (
     input   logic[3:0]	A,
     input   logic[3:0]	B,
-    input	logic		C_in
+    input	logic		C_in,
     output  logic[3:0] 	S,
     output  logic		C_out
 );
 
-	logic select, S0, S1, C0, C1;
-	adder4 A1 (.A (A), .B (B), .C_in (1'b0), .S (S0), .C_out (C0));
-	adder4 A2 (.A (A), .B (B), .C_in (1'b0), .S (S1), .C_out (C1));
+	logic select, C0, C1;
+	logic [3:0] S0, S1;
+	adder4 A1 (.A (A), .B (B), .C_in (C_in), .S (S0), .C_out (C0));
+	adder4 A2 (.A (A), .B (B), .C_in (C_in), .S (S1), .C_out (C1));
 
 	always_comb
+	if (C_in)
 	begin
-		if (C_in)
-			S = S1;
-			C_out = C1;
-		else
-			S = S0;
-			C_out = C0;
+		S = S1;
+		C_out = C1;
+	end
+	else
+	begin
+		S = S0;
+		C_out = C0;
 	end
 endmodule	
