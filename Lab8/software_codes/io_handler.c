f2ca4568075b99caba779a9912153a90fc15e959
rzhang56 <rzhang56@illinois.edu>
2018-03-09 16:29:46 -0600

    Implemented USB IO

--- Lab8/software_codes/io_handler.c
+++ Lab8/software_codes/io_handler.c
@@ -16,24 +16,32 @@ void IO_init(void)
 	*otg_hpi_reset = 1;
 	*otg_hpi_cs = 1;
 }
-
+/*
+ *	Active low, IO_write implementation to the OTG 
+ */
 void IO_write(alt_u8 Address, alt_u16 Data)
 {
-//*************************************************************************//
-//									TASK								   //
-//*************************************************************************//
-//							Write this function							   //
-//*************************************************************************//
+	// Init
+	*otg_hpi_cs = 0;				// Select chip (handshake)
+	*otg_hpi_address = Address;		// Populate address
+	*otg_hpi_data = Data;			// Populate data
+	// Control
+	*otg_hpi_w = 0;					// Write Enable
+	*otg_hpi_w = 1;
+	*otg_hpi_cs = 1;				// Terminate handshake
 }
 
 alt_u16 IO_read(alt_u8 Address)
 {
-	alt_u16 temp;
-//*************************************************************************//
-//									TASK								   //
-//*************************************************************************//
-//							Write this function							   //
-//*************************************************************************//
+	alt_u16 ret;
+
+	*otg_hpi_cs = 0;				// Select chip (handshake)
+	*otg_hpi_address = Address;		// Populate address
+	*otg_hpi_r = 0;
+
+	ret = *otg_hpi_data;
+	*otg_hpi_r = 1;
+	*otg_hpi_cs = 1;
 	//printf("%x\n",temp);
-	return temp;
+	return ret;
 }
