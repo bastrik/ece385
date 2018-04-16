ca10aff0880944ee5ed9a5cdf2aa612ed6dccb78
Justin Song <jmsong4@illinois.edu>
2018-04-15 20:27:31 -0500

    On-chip memory modules

---
+++ Project/player2_2.sv
@@ -0,0 +1,23 @@
+// On-chip memory 
+
+module player2_2(input logic clk,
+			 input logic [23:0] d,
+			 input logic [12:0] write_address, read_address,
+			 input we,
+			 output logic [23:0] q);
+
+	logic [23:0] mem [5625];
+
+	initial
+	begin
+		$readmemh("player2_2.txt", mem);
+	end
+
+	always_ff @ (posedge clk)
+	begin
+		if (we)
+			mem[write_address] <= d;
+		q <= mem[read_address];
+	end
+
+endmodule
---
+++ Project/sand.sv
@@ -0,0 +1,23 @@
+// On-chip memory 
+
+module sand(input logic clk,
+			 input logic [23:0] d,
+			 input logic [9:0] write_address, read_address,
+			 input we,
+			 output logic [23:0] q);
+
+	logic [23:0] mem [1024];
+
+	initial
+	begin
+		$readmemh("sand.txt", mem);
+	end
+
+	always_ff @ (posedge clk)
+	begin
+		if (we)
+			mem[write_address] <= d;
+		q <= mem[read_address];
+	end
+
+endmodule
